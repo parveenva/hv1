26bda6388f13256f5bd0e5b77f0b4737ff4c1b8b
code91co <code91.co@gmail.com>
2023-08-24 01:38:48 +0530

    Your commit message

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+
+  experimental: {
+
+    appDir: true
+    // Add the experimental features you want to enable here
+    // For example:
+    // concurrentFeatures: true,
+    // someOtherFeature: true,
+  },
 };
 
 module.exports = nextConfig;
